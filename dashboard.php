12f1f6d2d7fff10cd98008e0955170d269c2b642
Marco Antonio Fuentes Jimenez <Marco Antonio Fuentes Jimenez>
2016-05-09 01:57:49 -0500

    Video game implementation

--- dashboard.php
+++ dashboard.php
@@ -29,44 +29,7 @@
   <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
   <script src="js/jquery-1.12.3.min.js"></script>
   <!------Construct 2 ----------->
-  <script src="jquery-2.1.1.min.js"></script>
-  <script src="pathfind.js"></script>
-  <script src="c2runtime.js"></script>
 
-  <script>
-  // Size the canvas to fill the browser viewport.
-  jQuery(window).resize(function() {
-    cr_sizeCanvas(jQuery(window).width(), jQuery(window).height());
-  });
-
-  // Start the Construct 2 project running on window load.
-  jQuery(document).ready(function ()
-  {
-    // Create new runtime using the c2canvas
-    cr_createRuntime("c2canvas");
-  });
-
-  // Pause and resume on page becoming visible/invisible
-  function onVisibilityChanged() {
-    if (document.hidden || document.mozHidden || document.webkitHidden || document.msHidden)
-      cr_setSuspended(true);
-    else
-      cr_setSuspended(false);
-  };
-
-  document.addEventListener("visibilitychange", onVisibilityChanged, false);
-  document.addEventListener("mozvisibilitychange", onVisibilityChanged, false);
-  document.addEventListener("webkitvisibilitychange", onVisibilityChanged, false);
-  document.addEventListener("msvisibilitychange", onVisibilityChanged, false);
-
-  if (navigator.serviceWorker && navigator.serviceWorker.register)
-  {
-    // Register an empty service worker to trigger web app install banners.
-    navigator.serviceWorker.register("sw.js", { scope: "./" });
-  }
-  </script>
-
-  
 </head>
 <body>
   <div class="container container-fluid">
@@ -95,18 +58,60 @@
     </label>
     </div>
     <br>
-    <div class="row contenido jumbotron text-center">
-      <div id="c2canvasdiv">
-        <!-- The canvas the project will render to.  If you change its ID, don't forget to change the
-        ID the runtime looks for in the jQuery events above (ready() and cr_sizeCanvas()). -->
-        <canvas id="c2canvas" width="854" height="480">
-          <!-- This text is displayed if the visitor's browser does not support HTML5.
-          You can change it, but it is a good idea to link to a description of a browser
-          and provide some links to download some popular HTML5-compatible browsers. -->
-        </canvas>
+    <div id="c2canvasdiv" style="margin: 0 auto; width: 854px; height: 480px;">
 
-      </div>
+      <!-- The canvas the project will render to.  If you change its ID, don't forget to change the
+      ID the runtime looks for in the jQuery ready event (above). -->
+      <canvas id="c2canvas" width="854" height="480">
+        <!-- This text is displayed if the visitor's browser does not support HTML5.
+        You can change it, but it is a good idea to link to a description of a browser
+        and provide some links to download some popular HTML5-compatible browsers. -->
+        Your browser does not appear to support HTML5.  Try upgrading your browser to the latest version.  <a href="http://www.whatbrowser.org">What is a browser?</a>
+        <br/><br/><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx">Microsoft Internet Explorer</a><br/>
+        <a href="http://www.mozilla.com/firefox/">Mozilla Firefox</a><br/>
+        <a href="http://www.google.com/chrome/">Google Chrome</a><br/>
+        <a href="http://www.apple.com/safari/download/">Apple Safari</a><br/>
+        <a href="http://www.google.com/chromeframe">Google Chrome Frame for Internet Explorer</a><br/>
+      </canvas>
+
     </div>
   </div>
+  <script src="jquery-2.1.1.min.js"></script>
+  <script src="pathfind.js"></script>
+
+  <script src="c2runtime.js"></script>
+
+    <script>
+		// Size the canvas to fill the browser viewport.
+		jQuery(window).resize(function() {
+			cr_sizeCanvas(jQuery(window).width(), jQuery(window).height());
+		});
+
+		// Start the Construct 2 project running on window load.
+		jQuery(document).ready(function ()
+		{
+			// Create new runtime using the c2canvas
+			cr_createRuntime("c2canvas");
+		});
+
+		// Pause and resume on page becoming visible/invisible
+		function onVisibilityChanged() {
+			if (document.hidden || document.mozHidden || document.webkitHidden || document.msHidden)
+				cr_setSuspended(true);
+			else
+				cr_setSuspended(false);
+		};
+
+		document.addEventListener("visibilitychange", onVisibilityChanged, false);
+		document.addEventListener("mozvisibilitychange", onVisibilityChanged, false);
+		document.addEventListener("webkitvisibilitychange", onVisibilityChanged, false);
+		document.addEventListener("msvisibilitychange", onVisibilityChanged, false);
+
+		if (navigator.serviceWorker && navigator.serviceWorker.register)
+		{
+			// Register an empty service worker to trigger web app install banners.
+			navigator.serviceWorker.register("sw.js", { scope: "./" });
+		}
+    </script>
 </body>
 </html>
